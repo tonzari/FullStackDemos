089c4044587a86e9b451b85a08c3afec9d484fb6
Antonio Manzari <antoniomanzari@gmail.com>
2023-01-23 15:57:05 -0500

    added review demo

---
+++ Unit_02_Fundamental_Csharp/Demo_Review_EndOfWeek02/MethodReview.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExamples
+{
+    // These methods can be called from Program.cs 
+    // Usage: 
+    // string somePhoneNum = MethodReview.GetPhoneNumber();
+
+    public static class MethodReview
+    {
+        // Methods -- note: declaring methods HERE will cause them to be 'local functions' to the Program class. Normally, please write your methods in a separate class, in a separate file.
+
+        // Naming convention for Methods: PascalCase()
+        // Generally, method names are VerbNoun to express what the method does: like TryParse, WriteLine, GetNames, DisplayTask, ReadLine, UpdateEmailAddress, SetColor
+
+        public static void MakeGreen()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("The console? It's green now");
+        }
+
+        public static string GetPhoneNumber() // 248-123-1431 734.123.12312 (555)131-1231
+        {
+            return "123-123-1234";
+        }
+
+        // Overloading a method. Notice the Method name is the SAME, but now it has parameters
+        public static string GetPhoneNumber(string firstName, string lastName)
+        {
+            // checks database for a match of both first and lastname
+
+            return $"found phone number of {firstName} {lastName}: 1231231234";
+        }
+
+        public static string GetPhoneNumber(int userId)
+        {
+            // checks database for a match of userId
+            if (userId == 7)
+            {
+                return "found phone number: 1231231233";
+            }
+
+            return "user not found";
+        }
+    }
+}
---
+++ Unit_02_Fundamental_Csharp/Demo_Review_EndOfWeek02/Program.cs
@@ -0,0 +1,126 @@
+﻿using MethodExamples;
+
+// data types and variables
+
+//numbers
+double start = 3.456;
+decimal strong = 3.2m;
+decimal money = 7.1m;
+int length = 74;
+float boat = 22.5f;
+long wow = long.MinValue;
+ulong hello = ulong.MaxValue;
+
+bool isTrue = false;
+
+string color = "Howdy";
+char myLetter = 'a';
+
+const double PI = 3.14;
+const string DIRECTION = "NORTH";
+
+var someVariable = "string";
+var someOtherVariable = 123;
+var anotherVariable = 'c';
+
+
+
+
+// loops: for loop, do while, while, foreach
+
+for (int i = 0; i <= 10; i++)
+{
+    Console.Write($"{i}");
+}
+
+bool doAgain = false;
+do
+{
+    Console.WriteLine("I didn't copy this");
+} while (doAgain);
+
+
+while (false)
+{
+    Console.WriteLine("loop");
+}
+
+List<string> names = new List<string>()
+{
+    "Bella",
+    "Douglas",
+    "Priyanka",
+    "Opi"
+};
+
+foreach (string name in names)
+{
+    Console.WriteLine($"Hello, {name}!");
+}
+
+
+// Control statements: if, if/else, switch
+
+int num1 = 8;
+
+if (num1 == 9)
+{
+    // do the thing
+}
+
+if (true)
+{
+    // do this if true
+}
+else
+{
+    // otherwise do this
+}
+
+// do one of the following 
+if (true)
+{
+    // do this 
+}
+else if (true)
+{
+    // OR this
+}
+else
+{
+    // OR finally just do this
+}
+
+int caseSwitch = 1;
+switch (caseSwitch)
+{
+    case 1:
+        int age = 2;
+        Console.WriteLine(age);
+        break;
+    default:
+        Console.WriteLine("Default");
+        break;
+}
+
+// Methods
+
+MethodReview.MakeGreen();
+
+string phoneNum = MethodReview.GetPhoneNumber();
+Console.WriteLine(phoneNum);
+
+// overloaded versions of GetPhoneNumber()
+string personsPhoneNum = MethodReview.GetPhoneNumber("name1", "name2"); // this one takes two strings as arguments
+string otherPersonsNum = MethodReview.GetPhoneNumber(7);                // this one takes one int as an argument
+
+
+
+
+// hint on dice roller
+/*
+
+int dice1 = DiceRoller.RollDie(); // will return a value between 1 and 6
+int dice2 = DiceRoller.RollDie(); // run it again and store a second time
+
+ */
