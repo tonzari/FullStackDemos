00881b348d5aeae681c8dde3f4505c66fe564353
Antonio Manzari <antoniomanzari@gmail.com>
2023-02-15 22:04:27 -0500

    started SchoolApp demo

---
+++ Unit_04_Classes_And_Objects/Demo_SchoolApp/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Console.WriteLine("Welcome to the School Online System! Please login in to continue.");
+
+do
+{
+    Console.Write("Enter your email address: ");
+    string userEmail = Console.ReadLine();
+
+    Console.Write("Enter your password: ");
+    string userPassword = Console.ReadLine();
+
+    if (false)
+    {
+        // log the user in
+        // the whole app functions here.
+    }
+    else
+    {
+        Console.WriteLine("Sorry");
+    }
+
+    Console.WriteLine("Would you like to restart the App? [y]");
+
+} while (Console.ReadLine() == "y");
---
+++ Unit_04_Classes_And_Objects/Demo_SchoolApp/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_SchoolApp
+{
+    // In the context of our Application, a Student IS A User
+    public class Student : User 
+    {
+        public string Major { get; set; }
+    }
+}
