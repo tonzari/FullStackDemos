eedd0495f6d37f7758cf0b071ff7d66a355394d6
unknown <antoniomanzari@gmail.com>
2023-03-30 18:43:39 -0400

    added unit 7 MVC intro demo

---
+++ Unit_07_HTML_CSS_MVC_API/MVC_Demos/Demo_Intro/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Demo_Intro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Demo_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Let's add an About Me page
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
