89c2f342d9798eb47d2f254661969f9a566e1a30
Antonio Manzari <antoniomanzari@gmail.com>
2023-04-08 12:55:48 -0400

    removed lib

--- Unit_07_HTML_CSS_MVC_API/MVC_Demos/Demo_03_MVC_Forms_Validation/Controllers/HomeController.cs
+++ Unit_07_HTML_CSS_MVC_API/MVC_Demos/Demo_03_MVC_Forms_Validation/Controllers/HomeController.cs
@@ -32,20 +32,22 @@ namespace Demo_03_MVC_Forms_Validation.Controllers
             {
                 _userRepository.AddNewUser(userInfo);
 
-                return View("SubmitUserDetails", userInfo);
+                return View("Welcome", userInfo);
             }
 
+            // If the model is NOT valid, 
             return View(userInfo);
         }
 
-        public IActionResult SubmitUserDetails(UserInfo userInfo)
+        public IActionResult Welcome(UserInfo userInfo)
         {
             // Get saved user data from DB and display here. (We are using a mock DB for now.)
 
             UserInfo user = _userRepository.GetUserByEmail(userInfo.EmailAddress);
 
+            // If the user's email address wasn't found, then the DB didn't save the new user details. Thus the user object will be null.
             if (user != null) {
-                return View(user);
+                return View("Form");
             }
 
             return RedirectToAction("Index");
