7e82d600c11160da5cf0ae548253a7e1dbe9c058
Antonio Manzari <antoniomanzari@gmail.com>
2023-05-17 22:26:55 -0400

    finished service demo

--- Unit_08_JS_TS_ANG/Angular/Demo_03_Services_Intro/src/app/blog.service.ts
+++ Unit_08_JS_TS_ANG/Angular/Demo_03_Services_Intro/src/app/blog.service.ts
@@ -2,16 +2,35 @@ import { Injectable } from '@angular/core';
 
 import { Article } from './article';
 import { ARTICLES } from './mock-articles';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  getArticles(): Observable<Article[]> {
+  // This will not work in a real environment where you need to make HTTP requests to retrieve data
+  // NOTE: This method below was created for learning purposes. Please do not do this.
+
+  getArticlesSynchronous(): Article[] {
     
-    return ARTICLES; // to do! fix it!
+    return ARTICLES;
+  }
+
+  // Normally our service will be making HTTP requests to retreive data
+  // and that takes TIME!
+  // We will use rxjs and Observables to help with making asynchronous requests: https://rxjs.dev/
+
+  getArticles(): Observable<Article[]> {
+
+    // We want to turn our Article[] from the mock file into an Observable of Article[]
+    // We can make use of the rxjs method 'of'
+    const articles = of(ARTICLES);
+    
+    console.log(articles);
+
+    return articles;
+
   }
 
 }
--- Unit_08_JS_TS_ANG/Angular/Demo_03_Services_Intro/src/app/blog/blog.component.ts
+++ Unit_08_JS_TS_ANG/Angular/Demo_03_Services_Intro/src/app/blog/blog.component.ts
@@ -1,23 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit} from '@angular/core';
 import { Article } from '../article';
 import { BlogService } from '../blog.service';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.css'],
 })
-export class BlogComponent {
-    
-    constructor(private blogService: BlogService){}
+export class BlogComponent implements OnInit, AfterViewInit {
+  constructor(private blogService: BlogService) {
+    console.log('CONSTRUCTOR CALLED');
+  }
 
-    articles: Article[] = [];
+  // Code directly below is an example of a synchronous method retreiving data. please do not do this.
+  // articles: Article[] = this.blogService.getArticlesSynchronous();
 
-    searchArticles(substring: string): Article[] {
-        return this.articles.filter(article => 
-            article.title.toLowerCase().includes(substring.toLowerCase()) || 
-            article.excerpt.toLowerCase().includes(substring.toLowerCase())
-        );
-    }
+  /* - - - - - - - - Properties - - - - - - - -  */
+
+  articles: Article[] = [];
+  results: Article[] = [];
+
+
+
+  /* - - - - - - - - Life Cycle Hooks - - - - - - - -  */
+
+  ngOnInit(): void {
+    console.log('LIFE CYCLE HOOK: Blog component OnInit!');
+    this.getArticles();
+
+    this.results = this.articles;
+  }
+
+  // Just for demonstration. Life cycle hooks: https://angular.io/guide/lifecycle-hooks
+  ngAfterViewInit(): void {
+    console.log('LIFE CYCLE HOOK: after view init.');
+  }
+
+  /* - - - - - - - - Methods - - - - - - - -  */
+
+  getArticles(): void {
+    this.blogService.getArticles().subscribe(
+      // In Subscribe, you can define a function
+      // that will store the result of the subscription.
+      // Below, we have an arrow function. ex: x => console.log(x)
+      // ...where 'result' is the name of the parameter we've defined,
+      // and it will be passed an Artilce[]
+
+      (result: Article[]) => {
+        this.articles = result;
+        console.log(result);
+      }
+    );
+  }
+
+  testClick(): void {
+    console.log('clicked button!!');
+  }
+
+  filterArticles(searchString: string): void {
+    this.results = this.articles.filter(article => 
+        article.excerpt.toLowerCase().includes(searchString.toLowerCase()) ||
+        article.title.toLowerCase().includes(searchString.toLowerCase())
+    );
+  }
 
 }
