fb65b52a4221adf081b082c981f8d2490fab2184
Antonio Manzari <antoniomanzari@gmail.com>
2023-05-18 00:03:17 -0400

    added forms demo with design

---
+++ Unit_08_JS_TS_ANG/Angular/Demo_05_Forms_Designed/src/app/app-user.ts
@@ -0,0 +1,5 @@
+export interface AppUser {
+    firstname: string;
+    lastname: string;
+    email: string;
+}
---
+++ Unit_08_JS_TS_ANG/Angular/Demo_05_Forms_Designed/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EmailListComponent } from './email-list/email-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserFormComponent,
+    EmailListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
